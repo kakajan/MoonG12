e8b3eae68c36f448ba584d52e7a82540041f8463
U.Q SayyedMuhammadi <faslolkhitab@gmail.com>
2024-01-20 17:36:38 +0330

    VueJs Starter

---
+++ quasar/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page padding class="q-gutter-y-md">
+    <q-btn push icon="download" unelevated color="light-green-8" class="q-px-xl myBtn" label="Download" rounded />
+    <q-input prefix="+98" rounded outlined label="mobile" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>

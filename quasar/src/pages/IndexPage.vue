114f9b4544939402860fa01510fc3ee7df34367f
U.Q SayyedMuhammadi <faslolkhitab@gmail.com>
2024-01-27 17:38:44 +0330

    E4

--- quasar/src/pages/IndexPage.vue
+++ quasar/src/pages/IndexPage.vue
@@ -1,34 +1,32 @@
 <template>
   <q-page padding>
-    <div class="row q-gutter-md">
-      <div class="col-1">
-        <q-btn @click="counter>0?counter--:''" round icon="remove" color="pink-9" />
-      </div>
-      <div class="col">
-        <q-input v-model="counter" input-class="text-center" label="counter" />
-        {{ counter }}
-      </div>
-      <div class="col-1">
-        <q-btn @click="counter++" round icon="add" color="pink-9" />
+    <div v-if="messages.length > 0">
+      <div v-for="(message,index) in messages" :key="'message-'+index">
+      {{ message.fullName }}|{{ message.mobile }}<br>
+      {{ message.message }}<br>
+      <hr>
       </div>
     </div>
-    <q-btn @click="$router.push('/about')" label="About" />
   </q-page>
 </template>
 
 <script>
-import { defineComponent, onMounted, reactive, toRefs } from "vue";
+import axios from "axios";
+import { defineComponent, ref } from "vue";
 export default defineComponent({
   name: "IndexPage",
-  setup () {
-    const props = reactive({
-      counter: 0
-    })
-    onMounted(() => {
-    })
-    return {
-      ...toRefs(props),
+  setup() {
+    const messages = ref([]);
+    function fetchMessages() {
+      axios.get("http://localhost:8000/api/messages").then((r) => {
+        console.log(r.data);
+         messages.value = r.data;
+      });
     }
-  }
+    fetchMessages();
+    return {
+      messages,
+    };
+  },
 });
 </script>

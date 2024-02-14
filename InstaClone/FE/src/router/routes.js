0ae66c47356cc09bfdcf4e4830ccfcb710cc1b9f
U.Q SayyedMuhammadi <faslolkhitab@gmail.com>
2024-02-14 17:30:21 +0330

    E09

--- InstaClone/FE/src/router/routes.js
+++ InstaClone/FE/src/router/routes.js
@@ -1,47 +1,52 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      {
+        path: "",
+        component: () => import("pages/IndexPage.vue"),
+        meta: {
+          auth: true
+        },
+      },
+    ],
   },
   {
-    path: '/register',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/register",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('src/pages/Auth/RegisterPage.vue') }
-    ]
+      { path: "", component: () => import("src/pages/Auth/RegisterPage.vue") },
+    ],
   },
   {
-    path: '/login',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/login",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('src/pages/Auth/LoginPage.vue') }
-    ]
+      { path: "", component: () => import("src/pages/Auth/LoginPage.vue") },
+    ],
   },
   {
-    path: '/new-login',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/new-login",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('src/pages/Auth/NewLogin.vue') }
-    ]
+      { path: "", component: () => import("src/pages/Auth/NewLogin.vue") },
+    ],
   },
   {
-    path: '/confirm/:mobile',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/confirm/:mobile",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('src/pages/Auth/ConfirmLogin.vue') }
-    ]
+      { path: "", component: () => import("src/pages/Auth/ConfirmLogin.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
